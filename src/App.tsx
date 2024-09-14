04cf131c617883e548dd19910a9f324b5c82c186
Zhumakadyrova Nurzada <zhumakadyrova.nurzat081093@gmail.com>
2024-09-15 01:21:14 +0600

    Был реализован третий уровень данной лабораторной работы. А именно была добавлена кнопка которая раздает карты. Для раздачи карты была создана функция которая создает экземпляр полоды и выбирает рандомные пять карт и выводит на экран. На этом третий уровень выполнен!

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,45 @@
 import './App.css';
 import Card from './Components/Card/Card.tsx';
+import CardDeck from './lib/CardDeck.ts';
+import {useState} from 'react';
+
+interface ICardToPlay {
+    rank: string;
+    suit: string;
+}
 
 const App = () => {
 
+    const [someCards, setSomeCards] = useState<ICardToPlay[]>([]);
+
+    const cards = someCards.map((card, index) => {
+        return (
+            <Card key={index} rank={card.rank} suit={card.suit}/>
+        );
+    });
+
+    const getFifthCards = () => {
+        const cardDeck = new CardDeck();
+        const lastCards = cardDeck.getCards(5);
+        setSomeCards(lastCards);
+    };
+
+
     return (
-        <div className="playingCards faceImages">
-            <Card rank='5' suit="hearts"/>
-            <Card rank='K' suit="clubs"/>
-            <Card rank='10' suit="spades"/>
-            <Card rank='Q' suit="spades"/>
-        </div>
+        <>
+            {someCards.length === 0 ? (
+                <div>
+                    <button type="button" onClick={getFifthCards}>Раздать карты</button>
+                </div>
+            ) : (
+                <div>
+                    <div className="playingCards faceImages">{cards}</div>
+                    <div>
+                        <button type="button" onClick={getFifthCards}>Раздать карты</button>
+                    </div>
+                </div>
+            )}
+        </>
     );
 };
 
