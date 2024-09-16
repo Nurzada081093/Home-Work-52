4cab3a05a2babd8401cd9e14184c20abfd61b1b5
Zhumakadyrova Nurzada <zhumakadyrova.nurzat081093@gmail.com>
2024-09-16 13:10:33 +0600

    В файле PokerHand.ts были реализованы остальные части комбинаций.

--- src/lib/PokerHand.ts
+++ src/lib/PokerHand.ts
@@ -25,6 +25,9 @@ class PokerHand {
         let pairs:number = 0;
         let threes:number = 0;
         let fours:number = 0;
+        let sFlush = false;
+        let straight = false;
+        let RoyalFlush = false;
 
         for (const amountRankRepeat of amountRankRepeats) {
             if (amountRankRepeat === 2) {
@@ -43,11 +46,51 @@ class PokerHand {
         const getOneSuit = this.cards[0].suit;
         const getFlush = this.cards.every((card) => card.suit === getOneSuit);
 
+
+        if (getFlush) {
+            const rankNames:string[] = this.cards.map(card => card.rank);
+            const rankOrder:ICardHand = { '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10, 'J': 11, 'Q': 12, 'K': 13, 'A': 14 };
+            const rankNumbers:number[] = rankNames.map((rank:string) => {
+                return rankOrder[rank];
+            });
+            const sortRankValues:number[] = rankNumbers.sort((a:number, b:number) => a - b);
+            sFlush = sortRankValues.every((result:number, index:number, resultArray:number[]) => index === 0 || result === resultArray[index - 1] + 1);
+        }
+
+
+        if (getFlush) {
+            const rankRoyalFlushNames: string[] = this.cards.map(card => card.rank);
+            const rankRoyalFlushOrder: { [key: string]: number } = { '10': 10, 'J': 11, 'Q': 12, 'K': 13, 'A': 14 };
+            const rankRoyalFlushNumbers: number[] = rankRoyalFlushNames.map((rank:string) => rankRoyalFlushOrder[rank]);
+            const requiredRoyalFlushRanks: number[] = [10, 11, 12, 13, 14];
+            const sortRankRoyalFlushValues: number[] = rankRoyalFlushNumbers.sort((a:number, b:number) => a - b);
+            RoyalFlush = requiredRoyalFlushRanks.every((rank:number) => sortRankRoyalFlushValues.includes(rank));
+        }
+
+
+        if (getRankValue) {
+            const rankNamesValue:string[]= this.cards.map(card => card.rank);
+            const rankOrders:ICardHand = { '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10, 'J': 11, 'Q': 12, 'K': 13, 'A': 14 };
+            const ranksNumbers:number[] = rankNamesValue.map((rank:string) => {
+                return rankOrders[rank];
+            });
+            const sortRanksValues:number[] = ranksNumbers.sort((a:number, b:number) => a - b);
+            straight = sortRanksValues.every((result:number, index:number, resultArray:number[]) => index === 0 || result === resultArray[index - 1] + 1);
+        }
+
+
         if (this.cards.length === 5) {
-            if (threes > 0 && pairs > 0) {
+
+             if (RoyalFlush) {
+                return 'Royal flush';
+            } else if (threes > 0 && pairs > 0) {
                 return 'Full house';
+            } else if (sFlush) {
+                return 'Straight flush';
             } else if (getFlush) {
                 return 'Flush';
+            } else if (straight) {
+                return 'Straight';
             } else if (fours > 0) {
                 return 'Four of a kind';
             } else if (threes > 0) {
